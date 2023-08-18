f635c8f273102261e0f84fe8a8ca7a412993eef0
David Wright <david.wright@kofax.com>
2023-08-18 20:38:42 +0200

    Cache JSON into Rep so it persists to Extract Fixes #20

--- md/Project.vb
+++ md/Project.vb
@@ -12,45 +12,34 @@ Private Sub Document_BeforeClassifyXDoc(ByVal pXDoc As CASCADELib.CscXDocument, 
    'To trigger Microsoft Azure OCR in Kofax Transformation, rename the default page OCR profile to "Microsoft OCR"
    Dim DefaultPageProfileName As String
    DefaultPageProfileName=Project.RecogProfiles.ItemByID(Project.RecogProfiles.DefaultProfileIdPr).Name
-   If DefaultPageProfileName="Microsoft OCR" Then Microsoft_FormRecogniser(pXDoc)
+   If DefaultPageProfileName="Microsoft OCR" Then MicrosoftDI(pXDoc)
 End Sub
 
-Public Sub Microsoft_FormRecogniser(pXDoc As CscXDocument)
-   Dim EndPoint As String, Key As String, P As Long, RepName As String, StartTime As Long, Cache As String, JSON As String, Model As String
+Public Sub MicrosoftDI(pXDoc As CscXDocument)
+   Dim EndPoint As String, Key As String, RepName As String, StartTime As Long, Cache As String, JSON As String, Model As String, JS As Object
    RepName="MicrosoftOCR"
    'RepName="PDFTEXT"   'uncomment this line if you want Advanced Zone Locator to use Text
    While pXDoc.Representations.Count>0
       If pXDoc.Representations(0).Name=RepName Then Exit Sub 'We already have Microsoft OCR text, no need to call again.
       pXDoc.Representations.Remove(0) ' remove all OCR results from XDocument
    Wend
+   pXDoc.Representations.Create(RepName)
    EndPoint=Project.ScriptVariables.ItemByName("MicrosoftDocumentIntelligenceEndpoint").Value 'The Microsoft Azure Cloud URL
    Key=Project.ScriptVariables.ItemByName("MicrosoftDocumentIntelligenceKey").Value   'Key to use Microsoft Cognitive Services
-   Model=Project.ScriptVariables.ItemByName("MicrosoftDocumentIntelligenceModel").Value
-   Cache=Replace(pXDoc.FileName,".xdc", "." & Model & ".json")
-   If pXDoc.CDoc.Pages.Count=pXDoc.CDoc.SourceFiles.Count Then 'if the document has only single page files
-      For P=0 To pXDoc.CDoc.Pages.Count-1
-         If Dir(Cache)<>"" Then
-            JSON=File_Load(Cache)
-         Else
-            StartTime=Timer
-            JSON=MicrosoftFormRecogniser_REST(pXDoc.CDoc.Pages(P).SourceFileName,Model,EndPoint,Key,10)
-            'Store time in seconds that Microsoft took to read document
-            If pXDoc.XValues.ItemExists("MicrosoftOCR_Time") Then pXDoc.XValues.Delete("MicrosoftOCR_Time")
-            pXDoc.XValues.Add("MicrosoftOCR_Time",CStr(Timer-StartTime),True)
-            Open Cache For Output As #1
-            Print #1, vbUTF8BOM & JSON
-            Close #1
-         End If
-         Dim JS As Object
-         Set JS= JSON_Parse(JSON)
-         pXDoc.Representations.Create(RepName)
-         MicrosoftOCR_AddWords(pXDoc, JS, P)
-      Next
-      Exit Sub
+   JSON=Cache_Load(pXDoc,"MicrosoftDI_JSON")
+   If JSON="" Then
+      StartTime=Timer
+      JSON=MicrosoftDI_REST(pXDoc.CDoc.SourceFiles(0).FileName,Model,EndPoint,Key,10)
+      'Store time in seconds that Microsoft took to read document
+      If pXDoc.XValues.ItemExists("MicrosoftOCR_Time") Then pXDoc.XValues.Delete("MicrosoftOCR_Time")
+      pXDoc.XValues.Add("MicrosoftOCR_Time",CStr(Timer-StartTime),True)
+      Cache_Save(pXDoc,"MicrosoftDI_JSON",JSON)
    End If
+   Set JS= JSON_Parse(JSON)
+   MicrosoftDI_AddWords(pXDoc, JS, 0)
 End Sub
 
-Public Function MicrosoftFormRecogniser_REST(ImageFileName As String, Model As String, EndPoint As String, Key As String,Retries As Long) As String
+Public Function MicrosoftDI_REST(ImageFileName As String, Model As String, EndPoint As String, Key As String,Retries As Long) As String
    'Call Microsoft Azure Form Recognizer 3.0
    'https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/how-to-guides/use-sdk-rest-api?view=doc-intel-3.1.0&tabs=windows&pivots=programming-language-rest-api
    'model = prebuilt-document
@@ -99,7 +88,7 @@ Public Function MicrosoftFormRecogniser_REST(ImageFileName As String, Model As S
    Return HTTP.responseText
 End Function
 
-Public Sub MicrosoftOCR_AddWords(pXDoc As CscXDocument, JS As Object, PageOffset As Long)
+Public Sub MicrosoftDI_AddWords(pXDoc As CscXDocument, JS As Object, PageOffset As Long)
    Dim P As Long, W As Long, Key As String, Confidences As String, Word As CscXDocWord
    For P=0 To JS("js.analyzeResult.pages._count")-1
       For W=0 To JS("js.analyzeResult.pages(" & P & ").words._count")-1   'format
@@ -122,11 +111,11 @@ Public Sub MicrosoftOCR_AddWords(pXDoc As CscXDocument, JS As Object, PageOffset
    pXDoc.Representations(0).AnalyzeLines 'Redo Text Line Analysis in Kofax Transformation
 End Sub
 
-Public Sub MicrosoftOCR_AddTables(pXDoc As CscXDocument, JS As Object, PageOffset As Long)
+Public Sub MicrosoftDI_AddTables(pXDoc As CscXDocument, JS As Object, PageOffset As Long)
 
 End Sub
 
-Public Sub MicrosoftOCR_AddTable(pXDoc As CscXDocument, JS As Object, Table As CscXDocTable, T As Long)
+Public Sub MicrosoftDI_AddTable(pXDoc As CscXDocument, JS As Object, Table As CscXDocTable, T As Long)
    Dim Row As CscXDocTableRow, R As Long, C As Long, CellIndex As Long, Cell As CscXDocTableCell, W As Long, Words As CscXDocWords, P As Long, Key As String, BR As Long, BRKey As String
    Dim rowCount As Long, columnCount As Long
    Table.Rows.Clear
@@ -156,52 +145,12 @@ Public Sub MicrosoftOCR_AddTable(pXDoc As CscXDocument, JS As Object, Table As C
    Next
 End Sub
 
-Public Sub MicrosoftOCR_AddWords2(pXDoc As CscXDocument, JSON As String, PageOffset As Long)
-   'Microsoft OCR returns results in this format
-   '       {"content":"London","boundingBox":[1577.0,403.0,1643.0,404.0,1641.0,454.0,1575.0,453.0],"confidence":0.988,"span":{"offset":17,"length":4}}
-   Dim RegexPages As New RegExp, RegexWords As New RegExp, Confidences As String
-   Dim RegexLines As New RegExp
-   Dim pages As MatchCollection, P As Long, PageIndex As Long
-   Dim Words As MatchCollection, W As Long, BoundingBox() As String, Confidence As Double, Word As CscXDocWord
-   'RegexPages.Pattern="""pageNumber"":(\d+),""words"":\[({.*?})\],""spans"""   'returns pagenumber and words from JSON
-   RegexPages.Pattern="""pageNumber"":(\d+),.*?""words"":\[(.*?)\],""lines"""   'returns pagenumber and lines from JSON
-   RegexPages.Global=True ' Find more than one page!
-   'RegexWords.Pattern="""content"":""(.*?)"",""boundingBox"":\[(.*?)\],""confidence"":(.*?),""span"""  ' returns each word, boundingbox coordinates, confidence from a page
-   RegexWords.Pattern="""content"":""(.*?)"",""polygon"":\[(.*?)\],""confidence"":(\d\.\d+),"  ' returns each word, boundingbox coordinates, confidence from a page
-   RegexWords.Global=True ' find more than one word!
-   Set pages=RegexPages.Execute(JSON)
-   For P=0 To pages.Count-1
-      PageIndex=CLng(pages(P).SubMatches(0))-1 ' if a page is missing OCR it is possibe that PageNr is not the same as P.
-      Set Words = RegexWords.Execute(pages(P).SubMatches(1))
-      For W=0 To Words.Count-1 ' Create a Kofax Transformation word for each OCR word
-         Set Word = New CscXDocWord
-         Word.Text=JSON_Unescape( Words.Item(W).SubMatches(0))
-         Word.PageIndex=PageIndex+PageOffset
-         BoundingBox=Split(Words(W).SubMatches(1),",")' returns 8 numbers= 4 coordinates of topleft, topright, bottomright and bottomleft of word in pixels
-         'Microsoft OCR returns an irregular 4-edged polygon. Kofax Transformation requires a rectangle
-         Word.Left=min(CDouble(BoundingBox(0)), CDouble(BoundingBox(6)))
-         Word.Width=max(CDouble(BoundingBox(2)), CDouble(BoundingBox(4)))-Word.Left
-         Word.Top=min(CDouble(BoundingBox(1)),CDouble(BoundingBox(3)))
-         Word.Height=max(CDouble(BoundingBox(5)), CDouble(BoundingBox(7)))-Word.Top
-         Word.StringTag=Words(W).SubMatches(2)
-         ' We cannot set Word.Confidence directly from script, so we will store confidences in an XValue for the AZL
-         Confidences = Confidences & Words(W).SubMatches(2) & ","
-         pXDoc.Pages(PageIndex+PageOffset).AddWord(Word)
-      Next W
-   Next P
-   Confidences = Left(Confidences,Len(Confidences)-1) ' trim trailing ,
-   'Store all confidences for later use in AZL
-   If pXDoc.XValues.ItemExists("MicrosoftOCR_WordConfidences") Then pXDoc.XValues.Delete("MicrosoftOCR_WordConfidences")
-   pXDoc.XValues.Add("MicrosoftOCR_WordConfidences",Confidences,True)
-   pXDoc.Representations(0).AnalyzeLines 'Redo Text Line Analysis in Kofax Transformation
-End Sub
-
-Public Function min(a,b)
+Public Function min(A,b)
    'test
-   If a<b Then min=a Else min=b
+   If A<b Then min=A Else min=b
 End Function
-Public Function max(a,b)
-   If a>b Then max=a Else max=b
+Public Function max(A,b)
+   If A>b Then max=A Else max=b
 End Function
 
 Function CDouble(T As String) As Double
@@ -215,36 +164,65 @@ Function CDouble(T As String) As Double
    Return F.DoubleValue
 End Function
 
+
+Public Function Cache_Load(pXDoc As CscXDocument, RepName As String) As String
+   Dim R As Long, Model As String, CacheFileName As String
+   For R=0 To pXDoc.Representations.Count-1
+      If pXDoc.Representations(R).Name=RepName Then Return pXDoc.Representations(R).Text
+   Next
+   Model=Project.ScriptVariables.ItemByName("MicrosoftDocumentIntelligenceModel").Value
+   CacheFileName=Replace(pXDoc.FileName,".xdc", "." & Model & ".json")
+   If Dir(CacheFileName)<>"" Then Return File_Load(CacheFileName)
+End Function
+
+Public Sub Cache_Save(pXDoc As CscXDocument, RepName As String, Content As String)
+   'Cache a string
+   Dim R As Long, Model As String, CacheFileName As String
+   For R=pXDoc.Representations.Count-1 To 0 Step -1
+      If pXDoc.Representations(R).Name=RepName Then pXDoc.Representations.Remove(R)
+   Next
+   With pXDoc.Representations.Create(RepName).Words
+      Dim Word As New CscXDocWord
+      Word.Text=Content
+      .Append(Word)
+   End With
+   Model=Project.ScriptVariables.ItemByName("MicrosoftDocumentIntelligenceModel").Value
+   CacheFileName=Replace(pXDoc.FileName,".xdc", "." & Model & ".json")
+   Open CacheFileName For Output As #1
+   Print #1, vbUTF8BOM & Content
+   Close #1
+End Sub
+
 '-------------------------------------------------------------------
 ' VBA JSON Parser https://github.com/KofaxTransformation/KTScripts/blob/master/JSON%20parser%20in%20vb.md
 '-------------------------------------------------------------------
-Private t As Long, tokens() As String, dic As Object
+Private T As Long, tokens() As String, dic As Object
 Function JSON_Parse(JSON$, Optional Key$ = "js") As Object
-    t = 1
+    T = 1
     tokens = JSON_Tokenize(JSON)
     Set dic = CreateObject("Scripting.Dictionary")
-    If tokens(t) = "{" Then JSON_ParseObj(Key) Else JSON_ParseArr(Key)
+    If tokens(T) = "{" Then JSON_ParseObj(Key) Else JSON_ParseArr(Key)
     Return dic
 End Function
 Function JSON_ParseObj(Key$)
     Do
-      t = t + 1
-     Select Case tokens(t)
+      T = T + 1
+     Select Case tokens(T)
          Case "]"
          Case "[":  JSON_ParseArr(Key)
          Case "{"
-                    If tokens(t + 1) = "}" Then
-                        t = t + 1
+                    If tokens(T + 1) = "}" Then
+                        T = T + 1
                         dic.Add(Key, "null")
                     Else
                         JSON_ParseObj(Key)
                     End If
 
          Case "}":  Key = JSON_ParentPath(Key): Exit Do
-         Case ":":  Key = Key & "." & tokens(t - 1) 'previous token was a key - remember it
+         Case ":":  Key = Key & "." & tokens(T - 1) 'previous token was a key - remember it
          Case ",":  Key = JSON_ParentPath(Key)
          Case Else 'we are in a string. if next is not ":" then we are value - so add to dict!
-            If tokens(t + 1) <> ":" Then dic.Add(Key, tokens(t))
+            If tokens(T + 1) <> ":" Then dic.Add(Key, tokens(T))
      End Select
     Loop
 End Function
